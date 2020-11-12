4d13902e8888d988bba19882b235fa5fab3116e6
Jaime Ranchal <jaime.ranchal@gmail.com>
2020-11-12 16:32:10 +0100

    Ejercicio arrays y funciones en javascript

---
+++ js.online.03/disco.js
@@ -0,0 +1,38 @@
+export class Disco {
+    //propiedades
+    nombre;
+    autor;
+    fecha;
+    genero;
+    localización;
+    prestado = false;
+
+    //constructor
+    constructor(){}
+
+    //setters & getters
+    //methods
+    
+    /**
+     * Crea un nuevo disco
+     * Por defecto, prestado = false
+     */
+    function nuevoDisco() {
+
+    }
+
+    /**
+     * Cambia el número de estantería donde se guarda un disco
+     */
+    function cambiarLoc(){}
+
+    /**
+     * Cambia el estado de un disco a "Prestado" o viceversa
+     */
+    function cambiarEstado(){}
+
+    /**
+     * Muestra todas las propiedades de un disco
+     */
+    function mostrarDisco(){}
+}
