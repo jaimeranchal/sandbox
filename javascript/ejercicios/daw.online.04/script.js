cb23b3bead9e896b1a869eebc5e41b257bcc4b49
Jaime Ranchal <jaime.ranchal@gmail.com>
2021-01-05 16:02:39 +0100

    Añadido readme para tarea online 4 js. Actualización homepage

---
+++ javascript/ejercicios/daw.online.04/script.js
@@ -0,0 +1,32 @@
+"use strict"
+
+/* 2.Inserta una reCaptcha */
+
+/* 3.Cookies */
+// guarda número de intentos y
+// los muestra en el div#intentos
+
+/* 4.Convierte Nombre y Apellidos a mayúsculas */
+
+/* 5.Valida el email (regex) */
+// Si error: mensaje en span#errorEmail + no pierde foco
+
+/* 6.Valida el teléfono (keypress) */
+// sólo muestra números y espacios
+
+/* 7.Valida fecha llegada (regex) */
+// formato 'dd/mm/aaaa': regex explicado en comentarios
+// Bloquear el foco hasta que sea una fecha correcta
+// Si error: mensaje en span#errorfechaReserva
+
+/* 8.Valida fechaSalida (keypress) */
+// sólo muestra números y `/`
+// formato 'dd/mm/aaaa'
+// Bloquear el foco hasta que sea una fecha correcta
+// Si error: mensaje en span#errorfechaSalida
+
+/* 9.Comprobar datos requeridos no están vacíos (exc. observaciones) */
+// bucle elements[]
+// Si error: mensaje en span#error[Email| NomApe ...]
+
+/* 10. Muestra mensaje de confirmación de envío de formulario */
