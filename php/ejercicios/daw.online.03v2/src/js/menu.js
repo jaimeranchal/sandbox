32bd04f7b32e4f0e72a8d47ade989b70459a661a
Jaime Ranchal <jaime.ranchal@gmail.com>
2021-02-01 20:37:43 +0100

    Cambio automático del icono del botón de la sidebar según media-query

--- php/ejercicios/daw.online.03v2/src/js/menu.js
+++ php/ejercicios/daw.online.03v2/src/js/menu.js
@@ -1,10 +1,44 @@
 "use strict"
-// abre y cierra el menú en pantallas pequeñas
-let menu = document.getElementById("hamburger");
-let navegacion = document.getElementById("menu");
-menu.addEventListener('click', function(e){
-    // no puedo usar 'toggle' porque tienen más de una clase
-    // en su lugar: add("open") y remove("open")
-    navegacion.classList.toggle('open');
-    e.stopPropagation();
-})
+
+let botonSidebar = document.getElementById("menu-toggle");
+let mq = window.matchMedia("(min-width: 768px)");
+
+window.addEventListener('load', () => {
+    buttonIconStart(mq);
+    botonSidebar.addEventListener("click", toggleSidebar);
+    botonSidebar.addEventListener("click", buttonIconToggle);
+    // Invierte los iconos del menú lateral según esté escondido
+    // por defecto o no (regulado con media-queries)
+    mq.addEventListener('change', buttonIconToggle);
+});
+
+/* ------- Funciones -------- */
+
+//Muestra o esconde el menú izquierdo
+let toggleSidebar = (e) => {
+    let evento = e || event;
+    let wrapper = document.getElementById("wrapper");
+    evento.preventDefault();
+    wrapper.classList.toggle("toggled");
+
+}
+// cambia el icono del botón para el menú lateral
+let buttonIconToggle = () => {
+    let buttonIcon = document.getElementById("menu-toggle").firstChild;
+    let arrowLeft = 'fa-arrow-left';
+    let arrowRight = 'fa-arrow-right';
+    if (buttonIcon.classList.contains(arrowLeft)) {
+        buttonIcon.classList.replace(arrowLeft, arrowRight);
+    } else {
+        buttonIcon.classList.replace(arrowRight, arrowLeft);
+    }
+}
+
+//Carga los iconos del botón del menú lateral
+let buttonIconStart = (mq) => {
+    let arrowLeft = '<span class="fas fa-arrow-left"></span>';
+    let arrowRight = '<span class="fas fa-arrow-right"></span>';
+    botonSidebar.innerHTML = mq.matches ? arrowLeft : arrowRight;
+}
+
+
