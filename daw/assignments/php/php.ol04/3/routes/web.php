15529910a105098a3eb35f652cf1fea28a02f846
Jaime Ranchal <jaime.ranchal@gmail.com>
2021-02-23 16:48:18 +0100

    Proyecto en blanco + controlador con rutas

---
+++ daw/assignments/php/php.ol04/3/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/* Route::get('/', function () { */
+/*     return view('welcome'); */
+/* }); */
+
+Route::get("/","CustomController@index");
+Route::get("/crear",[CustomController::class, "create"]);
+Route::get("/articulos",[CustomController::class, "store"]);
+Route::get("/mostrar",[CustomController::class, "show"]);
