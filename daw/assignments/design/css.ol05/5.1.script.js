72e8b98f8607414e4960260bc5b0d9ee8c400aba
Jaime Ranchal <jaime.ranchal@gmail.com>
2021-03-04 11:23:55 +0100

    Figuras, patrones y transformación de imágenes

--- daw/assignments/design/css.ol05/5.1.script.js
+++ daw/assignments/design/css.ol05/5.1.script.js
@@ -1,44 +1,32 @@
 "use strict";
 
-let dibujarRombo = () => {
+let dibujarFigura = () => {
     let lienzo = document.getElementById("a");
-    let dibujo = lienzo.getContext("2d");
-    // Grosor de línea
-    dibujo.lineWidth = 5;
-    // Color de línea
-    dibujo.strokeStyle = "#212121";
-    // Color de relleno
-    dibujo.fillStyle = "#C2185B";
-    // Comenzamos la ruta de dibujo, o path
-    dibujo.beginPath();
-    // Nos movemos a la "punta" superior
-    dibujo.moveTo(55, 5);
-    // Dibujamos la línea que va hacia la derecha
-    dibujo.lineTo(105, 55);
-    // Ahora la que va hacia abajo
-    dibujo.lineTo(55, 105);
-    // Ahora la que va hacia la izquierda
-    dibujo.lineTo(5, 55);
-    // Y dejamos que la última línea la dibuje JS
-    dibujo.closePath();
-    // Hacemos que se dibuje
-    dibujo.stroke();
-    // Lo rellenamos
-    dibujo.fill(); 
+    let c = lienzo.getContext("2d");
+    // c.fillRect(25,25,100,100)
+    c.beginPath();
+    c.moveTo(75,25);
+    c.lineTo(125,125);
+    c.lineTo(25,125);
+    c.closePath();
+    c.stroke();
+
 }
 
 let dibujarPatron = () => {
 
     let lienzo = document.getElementById("b");
-    let dibujo = lienzo.getContext("2d");
+    let c = lienzo.getContext("2d");
 
     // usamos el objeto creado anteriormente como patrón
     let figura = document.getElementById("a");
-    let patron = dibujo.createPattern(figura, "repeat");
-    dibujo.rect(0,0,150,100);
-    dibujo.fillStyle = pat;
-    dibujo.fill();
+    let patron = c.createPattern(figura, "repeat");
+    c.fillStyle = patron;
+    c.fillRect(0,0,500,500);
 }
 
-window.addEventListener('load', dibujarRombo);
+window.addEventListener('load', () => {
+    dibujarFigura();
+    dibujarPatron();
+});
 
